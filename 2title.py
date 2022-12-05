4a8d5afe4e42282d8f5760427c6d273b7c184f25
Haiweed <119867410+Haiweed@users.noreply.github.com>
2022-12-05 18:46:26 +0300

    Add files via upload

---
+++ 2title.py
@@ -0,0 +1,33 @@
+titl="i love robots"
+print(titl.title())
+
+#Увеличение/уменьшение текста(методы) upper - большие lower - не большие)
+print(titl.upper())
+print(titl.lower()) 
+
+
+#Формат строки f- взятие переменной в письме
+first_name="gentoo"
+last_name="slackware"
+full_name = f"{first_name} {last_name}"
+print(f"i love u".upper(), f"{full_name}".title())
+name_mes=f"Hellow,  {full_name.title()}!"
+print(name_mes)
+
+#Табуляции /t /n (/n - разрывы строк)
+
+print("\thewwo".title())
+print("i love:\n\tPizza\n\tGames\n\tRobots")
+
+#Удаление пробелов метод .rstrip()- правый, lstrip() - левый, stip() - все
+
+fav_food = ' gedza '
+fav_food = fav_food.rstrip() #удаление пробела навсегда(правого)
+fav_food
+
+
+
+fav_food
+
+
+
