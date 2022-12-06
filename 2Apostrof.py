a791a644b18f7fd7bc8fcacf64dd7529aaf8cd24
Haiweed <119867410+Haiweed@users.noreply.github.com>
2022-12-06 23:13:10 +0300

    Add files via upload

---
+++ 2Apostrof.py
@@ -0,0 +1,18 @@
+message = "RUSSIA IS 'BEST' COUTNRY"
+print (message)
+
+#Управженения
+
+name = "eric"
+print (f"Hello {name.title()}! How are you?")
+
+name = "alex"
+print (name.upper(), name.lower(), name.title())
+
+famous_name = "Mark Twen"
+print (f"{famous_name} once said, 'Нет ничего проще, чем бросить курить! Я делал это сотни раз!'")
+message = f"{famous_name} once said, 'Нет ничего проще, чем бросить курить! Я делал это сотни раз!'" 
+print (message)
+
+name = "    Tape"
+print (name.lstrip())
